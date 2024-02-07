4cdeabeb0c078ae70b630ed81c35f5fa835d6671
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-02-06 22:50:05 -0300

    Finaliza método de fibonnaci

---
+++ recursao/src/Fibonacci.java
@@ -0,0 +1,13 @@
+public class Fibonacci {
+    /**
+     * Método que calcula o n-ésimo termo da sequência de Fibonacci
+     * @param n posição do termo desejado
+     * @return n-ésimo termo da sequência de Fibonacci
+     */
+    public static int fibonacci(int n){
+        if(n==0) return 0; //Caso de Parada
+        if(n==1) return 1; // Caso de Recursão
+
+        return fibonacci(n-1) + fibonacci(n-2); // Caso de Recursão. Recursão Binária
+    }
+}
