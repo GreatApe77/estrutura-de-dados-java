c7a436969792e84138627e2eab7d998caa6685c5
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-02-23 16:24:56 -0300

    figurinhas resolvida

---
+++ beecrowd/lista-av1/figurinhas/src/Main.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * IMPORTANT:
+ * O nome da classe deve ser "Main" para que a sua solução execute
+ * Class name must be "Main" for your solution to execute
+ * El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+    public static int calcularMDC(int a, int b) {
+        if (b == 0)
+            return a;
+        int resto = a % b;
+        a = b;
+        b = resto;
+        return calcularMDC(a, b);
+
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        Scanner scanner = new Scanner(System.in);
+        scanner.useDelimiter("\\n");
+        int entradaN = scanner.nextInt();
+
+        for (int i = 0; i < entradaN; i++) {
+            String entradaNumeros = scanner.next();
+            String[] entradaSeparadaPorEspacos =entradaNumeros.split(" ");
+            //System.out.println(Arrays.toString(entradaSeparadaPorEspacos));
+            String f1 = entradaSeparadaPorEspacos[0];
+            String f2 = entradaSeparadaPorEspacos[1];
+            int mdc = calcularMDC(Integer.parseInt(f1), Integer.parseInt(f2));
+            System.out.println(mdc);
+
+        }
+        scanner.close();
+        /**
+         * Escreva a sua solução aqui
+         * Code your solution here
+         * Escriba su solución aquí
+         */
+        /*
+         * String palavra = "Oi Bom Dia";
+         * String[] array = palavra.split(" ");
+         * for (int i = 0; i < array.length; i++) {
+         * System.out.println(array[i]);
+         * }
+         */
+    }
+
+}
