fd008926d0b9d0c6c53cb4d089263f3b1584e48d
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-03-10 15:59:15 -0300

    quicksort implementado

--- algoritmos/src/main/java/com/mateus/quicksort/QuickSort.java
+++ algoritmos/src/main/java/com/mateus/quicksort/QuickSort.java
@@ -3,117 +3,37 @@ package com.mateus.quicksort;
 import java.util.Arrays;
 
 public class QuickSort {
-    /*
-     * private static int _partition(int[] array, int inicio, int fim) {
-     * int pivot = array[fim];
-     * int i = inicio;
-     * for (int j = inicio; j <fim; j++) {
-     * if(array[j]<=pivot){
-     * int auxiliar = array[j];
-     * array[j] = array[i];
-     * array[i] = auxiliar;
-     * i++;
-     * }
-     * }
-     * int auxiliar = array[i];
-     * array[i] = array[fim];
-     * array[fim] = auxiliar;
-     * return i;
-     * }
-     * 
-     * public static void _quickSort(int[] array, int inicio, int fim) {
-     * if (fim == -1) {
-     * fim = array.length - 1;
-     * }
-     * if (inicio < fim) {
-     * int posicaoPivot = _partition(array, inicio, fim);
-     * _quickSort(array, inicio, posicaoPivot - 1);
-     * _quickSort(array, posicaoPivot + 1, fim);
-     * }
-     * }
-     * 
-     * public static void quickSort(int[] array) {
-     * int inicio = 0;
-     * int fim = -1;
-     * _quickSort(array, inicio, fim);
-     * 
-     * }
-     */
-    public static int _partitionCrescente(int[] array, int start, int end) {
-
-        int pivot = array[end];
-        int i = start - 1;
-        for (int j = start; j <= end - 1; j++) {
-            if (array[j] < pivot) {
+    private static int partition(int[] array,int comeco,int fim){
+        int pivot = array[fim];
+        int i =comeco-1;
+        for (int j = comeco; j <= fim-1 ; j++) {
+            if(array[j]<pivot){
                 i++;
-                int aux = array[i];
+                int auxiliar = array[i];
                 array[i] = array[j];
-                array[j] = aux;
-
+                array[j] = auxiliar;
             }
         }
         i++;
-        int aux = array[i];
-        array[i] = array[end];
-        array[end] = aux;
+        int auxiliar = array[i];
+        array[i] = array[fim]; //pivot
+        array[fim] = auxiliar;
         return i;
     }
-
-    public static void _quickSortCrescente(int[] array, int start, int end) {
-        if (end <= start) {
+    public static void quickSort(int[] array,int comeco,int fim){
+        if(fim<=comeco){
             return;
         }
-        int pivot = _partitionCrescente(array, start, end);
-        _quickSortCrescente(array, start, pivot - 1);
-        _quickSortCrescente(array, pivot + 1, end);
+        int pivot = partition(array, comeco, fim);
+        quickSort(array, comeco, pivot-1);
+        quickSort(array, pivot+1, fim); 
     }
-
-    public static void quickSortCrescente(int[] array) {
-        int start = 0;
-        int end = array.length - 1;
-        _quickSortCrescente(array, start, end);
-    }
-
-    public static int _partitionDecrescente(int[] array, int start, int end) {
-
-        int pivot = array[end];
-        int i = start - 1;
-        for (int j = start; j <= end - 1; j++) {
-            if (array[j] > pivot) {
-                i++;
-                int aux = array[i];
-                array[i] = array[j];
-                array[j] = aux;
-
-            }
-        }
-        i++;
-        int aux = array[i];
-        array[i] = array[end];
-        array[end] = aux;
-        return i;
-    }
-
-    public static void _quickSortDecrescente(int[] array, int start, int end) {
-        if (end <= start) {
-            return;
-        }
-        int pivot = _partitionDecrescente(array, start, end);
-        _quickSortDecrescente(array, start, pivot - 1);
-        _quickSortDecrescente(array, pivot + 1, end);
-    }
-
-    public static void quickSortDecrescente(int[] array) {
-        int start = 0;
-        int end = array.length - 1;
-        _quickSortDecrescente(array, start, end);
-    }
-
     public static void main(String[] args) {
-        int[] lista = new int[] { 4, 7, 2, 6, 4, 1, 8, 3 };
-        quickSortCrescente(lista);
-        System.out.println(Arrays.toString(lista));
-        quickSortDecrescente(lista);
+        int[] lista = new int[] { 4, 7, 2, 6, 4, 1, 8, 3,2000,9999,1,2,2,1,333,2,1,55,67,876,8,3 };
+        // quickSortCrescente(lista);
+        // System.out.println(Arrays.toString(lista));
+        // quickSortDecrescente(lista);
+        quickSort(lista, 0, lista.length-1);
         System.out.println(Arrays.toString(lista));
 
     }
