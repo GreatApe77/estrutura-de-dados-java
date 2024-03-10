717a9e8903e043b96e6310718db902c8dc575c4b
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-03-10 16:47:47 -0300

    metodo toString a ser implementadoc

--- implementacoes/lista-duplamente-encadeada/src/main/java/com/mateus/lista/duplamente/encadeada/ListaDuplamenteEncadeada.java
+++ implementacoes/lista-duplamente-encadeada/src/main/java/com/mateus/lista/duplamente/encadeada/ListaDuplamenteEncadeada.java
@@ -1,5 +1,40 @@
 package com.mateus.lista.duplamente.encadeada;
 
 public class ListaDuplamenteEncadeada<T> {
-    
+    private int tamanho;
+    private Nodo<T> primeiro;
+    private Nodo<T> ultimo;
+
+    public ListaDuplamenteEncadeada(){
+        this.tamanho= 0;
+        this.primeiro = null;
+        this.ultimo = null;
+    }
+
+    public void adicionarComeco(T elemento){
+        Nodo<T> novoNodo = new Nodo<T>(elemento);
+        if(this.tamanho==0){
+            this.primeiro = novoNodo;
+            this.ultimo = novoNodo;
+        }else{
+            primeiro.setAnterior(novoNodo);
+            novoNodo.setProximo(primeiro);
+            primeiro = novoNodo;
+        }
+    }
+    public void adicionarFinal(T elemento){
+
+    }
+    public void adicionar(int posicao, T elemento){
+
+    }
+
+
+    @Override
+    public String toString() {
+        
+        StringBuilder s = new StringBuilder();
+        s.append("[");
+        Nodo<T> atual = this.primeiro;
+    }
 }
