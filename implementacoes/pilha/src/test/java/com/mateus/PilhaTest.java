36842c33aeec3d8a7280d1da74202badd4e5d6b1
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-03-14 15:01:49 -0300

    testes de pilha

---
+++ implementacoes/pilha/src/test/java/com/mateus/PilhaTest.java
@@ -0,0 +1,47 @@
+package com.mateus;
+
+
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.mateus.pilha.IPilha;
+import com.mateus.pilha.Pilha;
+
+public class PilhaTest {
+
+   
+
+   
+   
+        
+  
+
+    @Test
+    public void testEmpilhar() {
+        IPilha<Integer> pilha = new Pilha<Integer>();
+        pilha.empilhar(11);
+        pilha.empilhar(222);
+        pilha.empilhar(333);
+
+        Assert.assertTrue(pilha.tamanho()==3);
+        Assert.assertTrue(pilha.topo()==333);
+    }
+
+    @Test
+    public void testDesempilhar() {
+        IPilha<Integer> pilha = new Pilha<Integer>();
+        Assert.assertTrue(pilha.tamanho()==0);
+        Assert.assertTrue(pilha.estaVazia());
+        pilha.empilhar(11);
+        Assert.assertFalse(pilha.estaVazia());
+        Integer elementoDesempilhado = pilha.desempilhar();
+        Assert.assertTrue(elementoDesempilhado==11);
+        Assert.assertTrue(pilha.tamanho()==0);
+        
+    }
+
+    
+   
+}
