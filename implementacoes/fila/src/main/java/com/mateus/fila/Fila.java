6079c1da6f921c81b75aab9af1ca5683f7795284
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-03-14 15:08:49 -0300

    interface da fila

---
+++ implementacoes/fila/src/main/java/com/mateus/fila/Fila.java
@@ -0,0 +1,5 @@
+package com.mateus.fila;
+
+public class Fila {
+    
+}
---
+++ implementacoes/fila/src/main/java/com/mateus/fila/Nodo.java
@@ -0,0 +1,32 @@
+package com.mateus.fila;
+
+class Nodo<T> {
+    private Nodo<T> anterior;
+    private Nodo<T> proximo;
+    private T elemento;
+    
+    public Nodo(T elemento){
+        this.anterior = null;
+        this.proximo = null;
+        this.elemento = elemento;
+    }
+
+    public T getElemento() {
+        return elemento;
+    }
+    public void setElemento(T elemento) {
+        this.elemento = elemento;
+    }
+    public void setAnterior(Nodo<T> anterior) {
+        this.anterior = anterior;
+    }
+    public void setProximo(Nodo<T> proximo) {
+        this.proximo = proximo;
+    }
+    public Nodo<T> getAnterior() {
+        return anterior;
+    }
+    public Nodo<T> getProximo() {
+        return proximo;
+    }
+}
