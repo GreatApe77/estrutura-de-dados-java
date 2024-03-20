1b3dade746c4432b67b402162b9b4820a286e251
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-03-20 16:03:09 -0300

    metodo de inserir ordenadoc

--- rascunhos/revisao-av1/src/Main.java
+++ rascunhos/revisao-av1/src/Main.java
@@ -8,10 +8,22 @@ public class Main {
         // System.out.println("Hello, World!");
         // ListaEstatica<Integer> listaEstatica = new ListaEstatica<Integer>(6);
         ListaEncadeada<Integer> lista = new ListaEncadeada<Integer>();
-        for (int i = 1; i <= 10; i++) {
-            // listaEstatica.inserir(i);
-            lista.inserirFinal(i);
-        }
+        // for (int i = 1; i <= 10; i++) {
+        // listaEstatica.inserir(i);
+        // lista.inserirFinal(i);
+        // }
+        //lista.inserirFinal(80);
+        //lista.inserirFinal(77);
+        //lista.inserirComeco(66);
+            lista.insereOrdenado(77);
+            lista.insereOrdenado(7);
+            lista.insereOrdenado(1);
+            lista.insereOrdenado(2);
+            lista.insereOrdenado(77);
+            lista.insereOrdenado(77);
+            lista.insereOrdenado(99);
+            
+
         System.out.println(lista);
     }
 }
--- rascunhos/revisao-av1/src/entities/ListaEncadeada.java
+++ rascunhos/revisao-av1/src/entities/ListaEncadeada.java
@@ -39,6 +39,51 @@ public class ListaEncadeada<T> {
         this.ultimo = null;
         this.tamanho=0;
     }
+    
+    public void insereOrdenado(T elemento){
+        //Comparable == 0
+        // Comparable > 1
+        //Comparable < -1
+        //Nodo<T> novoNodo = new Nodo<T>(elemento);
+        if(this.primeiro==null||((Comparable) elemento).compareTo(this.primeiro.getElemento())==-1 ||((Comparable) elemento).compareTo(this.primeiro.getElemento())==0){
+            inserirComeco(elemento);
+            return;
+        }else if((((Comparable) elemento).compareTo(this.ultimo.getElemento())==1)||((Comparable) elemento).compareTo(this.ultimo.getElemento())==0){
+            inserirFinal(elemento);
+            return;
+        }
+        Nodo<T> novoNodo = new Nodo<T>(elemento);
+        Nodo<T> atual = this.primeiro;
+        while (atual.getProximo()!=null && (((Comparable) elemento).compareTo(atual.getProximo().getElemento())==1) ) {
+            atual = atual.getProximo();
+        }
+        novoNodo.setProximo(atual.getProximo());
+        atual.setProximo(novoNodo);
+        _incrementarTamanho();
+    }
+    public void inserirComeco(T elemento){
+        Nodo<T> novoNodo = new Nodo<T>(elemento);
+        if(estaVazia()){
+            this.primeiro = novoNodo;
+            this.ultimo = novoNodo;
+            _incrementarTamanho(); 
+            return;
+        }
+        if(tamanho()==1){
+            novoNodo.setProximo(this.primeiro);
+            this.primeiro=novoNodo;
+            _incrementarTamanho();
+            return;
+            //this.primeiro.setProximo(primeiro);
+            //novoNodo.setProximo(this.primeiro);
+            //this.primeiro = novoNodo;
+            //_incrementarTamanho();
+        }
+        
+        novoNodo.setProximo(this.primeiro);
+        this.primeiro = novoNodo;
+        _incrementarTamanho();
+    }
     public void inserirFinal(T elemento){
         Nodo<T> novoNodo = new Nodo<T>(elemento);
         if(estaVazia()){
