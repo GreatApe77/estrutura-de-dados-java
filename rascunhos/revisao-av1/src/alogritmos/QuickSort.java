caec73d6da17cd41ecd5dd4b8340a736d90d30d2
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-03-21 01:00:20 -0300

    quick sort 100 por cento recursivo

--- rascunhos/revisao-av1/src/alogritmos/QuickSort.java
+++ rascunhos/revisao-av1/src/alogritmos/QuickSort.java
@@ -15,18 +15,32 @@ public class QuickSort {
     private static int _partition(int[] vetor,int esquerda,int direita){
         int elementoPivot = vetor[direita];
         int i = esquerda-1;
-        for (int j = esquerda; j <= direita-1; j++) {
-            if(vetor[j]<elementoPivot){
-                i++;
-                int aux = vetor[j];
-                vetor[j] = vetor[i];
-                vetor[i] = aux;
-            }
-        }
+        int j = esquerda;
+        //for (int j = esquerda; j <= direita-1; j++) {
+         //   if(vetor[j]<elementoPivot){
+        ////        i++;
+        //        int aux = vetor[j];
+        //        vetor[j] = vetor[i];
+        //        vetor[i] = aux;
+        //    }
+       // }
+        i = testeRecursivo(vetor, j, i, direita, elementoPivot);
         i++;
         int aux = vetor[i];
         vetor[i] = vetor[direita];
         vetor[direita] = aux;
         return i;
+    }
+    private static int testeRecursivo(int[] vetor,int j,int i,int direita,int elementoPivot){
+        if( j>direita-1){
+            return i;
+        }
+        if(vetor[j]<elementoPivot){
+            i++;
+            int aux = vetor[j];
+            vetor[j] = vetor[i];
+            vetor[i] = aux;
+        }
+        return testeRecursivo(vetor, j+1, i, direita, elementoPivot);
     }
 }
