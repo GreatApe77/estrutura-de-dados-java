98ceb7f91a0d96abdc615848c8378e49ac0fb433
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-04-06 13:11:21 -0300

    parenthesis validator

---
+++ rascunhos/validador-de-expressoes/src/ExpressionValidator.java
@@ -0,0 +1,21 @@
+public class ExpressionValidator {
+
+    public static boolean validate(String expression) {
+        StaticStack<String> stack = new StaticStack<String>(expression.length());
+        char openParenthesis = '(';
+        char closedParenthesis = ')';
+        boolean containsParenthesis = false;
+        for (int i = 0; i < expression.length(); i++) {
+            char currentChar = expression.charAt(i);
+            if(currentChar==openParenthesis){
+                stack.push("(");
+                containsParenthesis = true;
+            }else if(expression.charAt(i)==closedParenthesis){
+                if(stack.isEmpty()) return false;
+                stack.pop();
+                containsParenthesis = true;
+            }
+        }
+        return containsParenthesis && stack.isEmpty();
+    }
+}
