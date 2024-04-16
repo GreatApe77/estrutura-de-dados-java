5c54a2cfada6d4219d2eed5e9a3f805757700023
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-04-16 17:27:17 -0300

    cria interface

---
+++ implementacoes/fila-dinamica/src/main/java/com/mateus/filaDinamica/FilaDinamica.java
@@ -0,0 +1,16 @@
+package com.mateus.filaDinamica;
+
+public class FilaDinamica<T> {
+    
+    public boolean estaVazia(){}
+    
+    public T frente(){}
+    public void enfileira(T elemento){}
+    public T desenfileira(){}
+    public int tamanho(){}
+    @Override
+    public String toString() {
+        
+    }
+    
+}
