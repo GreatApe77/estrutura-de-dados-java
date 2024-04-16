5b69c733bbeec221108d5e1354b71760ce582cfc
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-04-16 17:46:07 -0300

    enfileira funciona

--- implementacoes/fila-dinamica/src/main/java/com/mateus/App.java
+++ implementacoes/fila-dinamica/src/main/java/com/mateus/App.java
@@ -1,13 +1,17 @@
 package com.mateus;
 
+import com.mateus.filaDinamica.FilaDinamica;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        FilaDinamica<String> filaDinamica = new FilaDinamica<String>();
+        filaDinamica.enfileira("Mateus");
+        filaDinamica.enfileira("Manoel Carlos");
+        filaDinamica.enfileira("Kiko");
+        System.out.println(filaDinamica);
     }
 }
