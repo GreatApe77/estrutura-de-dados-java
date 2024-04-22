0be78d06a54318d3ed6491412ac745d4d8ee39e7
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-04-22 18:56:28 -0300

    Adiciona implementacao do elemento

--- implementacoes/fila-prioridade-estatica-heap/src/main/java/com/mateus/filaPrioridadeHeap/FilaPrioridadeHeap.java
+++ implementacoes/fila-prioridade-estatica-heap/src/main/java/com/mateus/filaPrioridadeHeap/FilaPrioridadeHeap.java
@@ -1,5 +1,55 @@
 package com.mateus.filaPrioridadeHeap;
 
-public class FilaPrioridadeHeap {
+public class FilaPrioridadeHeap<Chave,Valor> implements FilaPrioridade<Chave,Valor> {
+    
+    
+     class ElementoDaFila implements Elemento<Chave,Valor> {
+        Chave chave;
+        Valor valor;
+        
+        @Override
+        public Chave getChave() {
+            return this.chave;
+        }
+        @Override
+        public Valor getValor() {
+            return this.valor;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + chave + " - " + valor + ")";
+        }
+        
+    }
+    @Override
+    public void insere(Chave chave, Valor valor) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'insere'");
+    }
+
+    @Override
+    public Elemento<Chave, Valor> frente() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'frente'");
+    }
+
+    @Override
+    public Elemento<Chave, Valor> remove() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'remove'");
+    }
+
+    @Override
+    public int tamanho() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'tamanho'");
+    }
+
+    @Override
+    public boolean estaVazia() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'estaVazia'");
+    }
     
 }
