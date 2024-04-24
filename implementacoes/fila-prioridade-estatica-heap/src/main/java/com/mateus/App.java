1e68d9292a10e118b3e76dd624580e5a3a61be98
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-04-24 14:27:33 -0300

    prioridade nao esta sendo respeitada para itens de mesma prioridade

--- implementacoes/fila-prioridade-estatica-heap/src/main/java/com/mateus/App.java
+++ implementacoes/fila-prioridade-estatica-heap/src/main/java/com/mateus/App.java
@@ -14,19 +14,26 @@ public class App {
         ComparadorPadrao<Integer> comp = new ComparadorPadrao<Integer>();
         System.out.println(comp.compare(70, 80));
         FilaPrioridadeHeap<Integer, String> fila = new FilaPrioridadeHeap<Integer, String>(30);
-        fila.insere(50, "Marcos");
-        fila.insere(60, "Paulo");//
-        fila.insere(50, "Jonas");
-        fila.insere(49, "Carlin");//
-        fila.insere(1, "Mateus");
-        fila.insere(50, "Rafael");
-        fila.insere(999, "Maria");//
-        fila.insere(61, "Claudio");//
-        fila.insere(60, "Fernando");//
+        /*
+         * fila.insere(50, "Marcos");
+         * fila.insere(60, "Paulo");//
+         * fila.insere(50, "Jonas");
+         * fila.insere(49, "Carlin");//
+         * fila.insere(1, "Mateus");
+         * fila.insere(50, "Rafael");
+         * fila.insere(999, "Maria");//
+         * fila.insere(61, "Claudio");//
+         * fila.insere(60, "Fernando");//
+         */
+        fila.insere(1, "Marcos");
+        fila.insere(1, "Geraldo");
+        fila.insere(1, "Carla");
         System.out.println(fila.remove());
         System.out.println(fila.remove());
-        System.out.println(fila.remove());
-        System.out.println(fila.remove());
+        // System.out.println(fila.remove());
+        // System.out.println(fila.remove());
+        // System.out.println(fila.remove());
+        // System.out.println(fila.remove());
         // System.out.println(fila.remove());
 
     }
