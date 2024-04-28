b48a8ea94e4bd8b22601e65547315f9b79e8f172
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-04-28 17:19:36 -0300

    logica de fatoracao

---
+++ algoritmos/src/main/java/com/mateus/fatoracao/Fatoracao.java
@@ -0,0 +1,27 @@
+package com.mateus.fatoracao;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Fatoracao {
+    public static void fatoracao(int n){
+        Queue<Integer> fila = new LinkedList<Integer>();
+        for (int i = 2; i < n/i; i++) {
+            while (n%i==0){
+                fila.add(i);
+                n = n/i;
+            }
+        }
+        if (n > 1) {
+            fila.add(n);
+        }
+        int size = fila.size();
+        for (int i = 0; i < size; i++) {
+            System.out.println(fila.remove());
+        }
+    }
+
+    public static void main(String[] args) {
+        fatoracao(3960);
+    }
+}
