3ee54af17b27799ebf71146a3aa07525e31f87c1
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2024-04-28 10:48:21 -0300

    fila Dinamica

--- rascunhos/revisao-av2/src/App.java
+++ rascunhos/revisao-av2/src/App.java
@@ -1,21 +1,39 @@
+import fila.FilaDinamica;
 import fila.FilaEstaticaCircular;
 import pilha.PilhaDinamica;
 import pilha.PilhaEstatica;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        PilhaDinamica<String> pilhaDeLivros = new PilhaDinamica<String>();
-        pilhaDeLivros.empilha("Senhor dos Aneis");
-        // pilhaDeLivros.empilha("AOT");
-        // pilhaDeLivros.empilha("Apostila");
-        System.out.println(pilhaDeLivros);
-        String desempilhado = pilhaDeLivros.desempilha();
-        System.out.println(desempilhado);
-        System.out.println(pilhaDeLivros.topo());
-        System.out.println(pilhaDeLivros);
-        pilhaDeLivros.empilha(desempilhado);
-        System.out.println(pilhaDeLivros);
-        pilhaDeLivros.empilha("Bom dia");
-        System.out.println(pilhaDeLivros);
+        FilaDinamica<String> fila = new FilaDinamica<String>();
+        fila.enfileira("Mateus");
+        fila.enfileira("Davi");
+        String saiuDaFila = fila.desenfileira();
+        String saiuDaFila2 = fila.desenfileira();
+        System.out.println("SAIU: "+saiuDaFila);
+        System.out.println("SAIU: "+saiuDaFila2);
+        System.out.println(fila.frente());
+        fila.enfileira("Mateus");
+        fila.enfileira("Davi");
+         saiuDaFila = fila.desenfileira();
+         saiuDaFila2 = fila.desenfileira();
+        System.out.println("SAIU: "+saiuDaFila);
+        System.out.println("SAIU: "+saiuDaFila2);
+        System.out.println(fila.frente());
+        fila.enfileira("Mateus");
+        fila.enfileira("Davi");
+         saiuDaFila = fila.desenfileira();
+         saiuDaFila2 = fila.desenfileira();
+        System.out.println("SAIU: "+saiuDaFila);
+        System.out.println("SAIU: "+saiuDaFila2);
+        System.out.println(fila.frente());
+        fila.enfileira("Mateus");
+        fila.enfileira("Davi");
+         saiuDaFila = fila.desenfileira();
+         saiuDaFila2 = fila.desenfileira();
+        System.out.println("SAIU: "+saiuDaFila);
+        System.out.println("SAIU: "+saiuDaFila2);
+        System.out.println(fila.frente());
+        
     }
 }
