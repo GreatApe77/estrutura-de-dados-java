52e60e0031f6207139319283e67adb724f5a238e
Mateus Arruda Navarro Albuquerque <mateusnavarro9@gmail.com>
2025-02-19 08:17:02 -0300

    classic implementation for insertion sort

--- algoritmos/src/main/java/com/mateus/insertionsort/InsertionSort.java
+++ algoritmos/src/main/java/com/mateus/insertionsort/InsertionSort.java
@@ -4,13 +4,13 @@ public class InsertionSort {
     public static void insertionSort(int[] list ){
         if(list.length<=1) return;
         for (int i = 1; i < list.length; i++) {
-            int pointer = i;
-            while (  pointer>=1 && list[pointer]<list[pointer-1]) {
-                int aux = list[pointer];
-                list[pointer] = list[pointer-1];
-                list[pointer-1] = aux;
-                pointer--;
+            int key = list[i];
+            int j = i-1;
+            while (  j>=0  &&key<list[j]) {
+                list[j+1]= list[j];
+                j--;
             }
+            list[j+1] = key;
         }
     }
 }
